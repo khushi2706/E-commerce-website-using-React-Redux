dd26845851b0ace2fa37ac1d043f30e5f865c5bf
rajveer43 <rajveer.rathod1301@gmail.com>
2024-03-28 21:43:37 +0530

    set env key

--- src/genai/conversation.py
+++ src/genai/conversation.py
@@ -8,6 +8,10 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
 def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
     chunks = text_splitter.split_text(text)
